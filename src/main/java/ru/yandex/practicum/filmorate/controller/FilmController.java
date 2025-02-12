7a0a0a5d385f12f70f3a8cac8e059c1217f93014
Bequitebtw <Bequitebtw@mail.ru>
2025-02-12 12:00:03 +0300

    create Film and User controller + tests

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,64 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    HashMap<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film createFilm(@Valid @RequestBody Film film) {
+        filmReleaseDateValidation(film);
+        film.setId(getNextId());
+        films.put(getNextId(), film);
+        log.info("{} добавлен!", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (film.getId() == null) {
+            log.warn("Название фильме не может быть пустым");
+            throw new ValidationException("Название фильме не может быть пустым");
+        }
+        if (!films.containsKey(film.getId())) {
+            log.warn("Фильма с id {} не существует", film.getId());
+            throw new ValidationException("Фильма с таким id не существует");
+        }
+        filmReleaseDateValidation(film);
+        Film oldFilm = films.get(film.getId());
+        oldFilm.setId(film.getId());
+        oldFilm.setName(film.getName());
+        oldFilm.setDescription(film.getDescription());
+        oldFilm.setReleaseDate(film.getReleaseDate());
+        oldFilm.setDuration(film.getDuration());
+        log.info("{} обновлен!", film);
+        return oldFilm;
+    }
+
+    private void filmReleaseDateValidation(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Дата релиза не может быть раньше 28 декабря 1895 года");
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
+
+    private int getNextId() {
+        return films.keySet().stream().mapToInt(Integer::intValue).max().orElse(0) + 1;
+    }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTests.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Не знаю какие еще можно написать тесты для pojo, тесты в postman`е покрывают всю валидацию в контроллерах и классах
+public class FilmTests {
+
+    private Film film;
+
+    @BeforeEach
+    public void createNewFilm() {
+        film = new Film("name", "desk", LocalDate.of(1900, 12, 12), 200L);
+    }
+
+    @Test
+    void shouldCreateFilmWithValidData() {
+        assertNull(film.getId());
+        assertNotNull(film.getName());
+        assertNotNull(film.getDescription());
+        assertNotNull(film.getReleaseDate());
+        assertNotNull(film.getDuration());
+    }
+
+    @Test
+    void shouldSetFilmCorrectFields() {
+        film.setName("Name1");
+        film.setDescription("Desk1");
+        film.setReleaseDate(LocalDate.of(2004, 5, 10));
+        film.setId(1);
+        film.setDuration(100L);
+        assertEquals(film.getName(), "Name1");
+        assertEquals(film.getDescription(), "Desk1");
+        assertEquals(film.getReleaseDate(), LocalDate.of(2004, 5, 10));
+        assertEquals(film.getId(), 1);
+        assertEquals(film.getDuration(), 100L);
+    }
+
+}
